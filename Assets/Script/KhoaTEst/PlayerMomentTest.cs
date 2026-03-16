7f04bab9d2c917f1c35bbb59e26e70e810611623
khoatnd223 <243987557+KhoaCodeBug@users.noreply.github.com>
2026-03-16 21:33:28 +0700

    Asset2D

---
+++ Assets/Script/KhoaTEst/PlayerMomentTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMomentTest : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // lấy input từ bàn phím
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // di chuyển player
+        rb.linearVelocity = movement.normalized * moveSpeed;
+    }
+}
