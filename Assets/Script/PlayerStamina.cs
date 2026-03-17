9848ad14879b268fd0ab6a0cd3906bb5f8c7dd41
NguyenTriTin3008 <tritin.caolanh@gmail.com>
2026-03-17 19:53:37 +0700

    prefab

---
+++ Assets/Script/PlayerStamina.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerStamina : MonoBehaviour
+{
+    [Header("--- Stamina System ---")]
+    public float maxStamina = 100f;
+    public float currentStamina = 100f;
+    public float staminaDrain = 20f;       // Mất bao nhiêu Stamina mỗi giây khi chạy
+
+    [Tooltip("Tốc độ hồi Stamina khi ĐỨNG YÊN thở")]
+    public float staminaRecoverIdle = 15f;
+
+    [Tooltip("Tốc độ hồi Stamina khi ĐI BỘ/LẾT")]
+    public float staminaRecoverWalk = 5f;
+
+    // Biến này được đóng gói (Encapsulation), các script khác chỉ được ĐỌC chứ không được SỬA
+    public bool IsExhausted { get; private set; } = false;
+
+    // Hàm này sẽ được PlayerMovement gọi liên tục để cập nhật thể lực
+    public void UpdateStamina(bool isRunning, bool isMovingNow)
+    {
+        // 1. Trừ thể lực khi đang chạy
+        if (isRunning && isMovingNow)
+        {
+            currentStamina -= staminaDrain * Time.deltaTime;
+            if (currentStamina <= 0)
+            {
+                currentStamina = 0;
+                IsExhausted = true; // Cạn kiệt thể lực
+            }
+        }
+        // 2. Hồi thể lực khi không chạy (Đi bộ hoặc Đứng yên)
+        else
+        {
+            // Chọn tốc độ hồi phục tương ứng
+            float currentRecoverRate = isMovingNow ? staminaRecoverWalk : staminaRecoverIdle;
+
+            // Cộng thể lực
+            currentStamina += currentRecoverRate * Time.deltaTime;
+
+            if (currentStamina >= maxStamina)
+            {
+                currentStamina = maxStamina;
+                IsExhausted = false; // Đầy thể lực, sẵn sàng chạy tiếp
+            }
+        }
+    }
+}
