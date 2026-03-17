bb6a58c9e1fa48b60303b1094aa1486bc3a9cb7c
NguyenTriTin3008 <tritin.caolanh@gmail.com>
2026-03-17 22:43:22 +0700

    inventory

---
+++ Assets/Script/InventorySlotUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro; // Dùng dòng này nếu bạn xài TextMeshPro, nếu xài Text thường thì xóa đi
+
+public class InventorySlotUI : MonoBehaviour
+{
+    public Image itemIcon;
+    public TextMeshProUGUI amountText; // Nếu xài Text thường thì đổi thành: public Text amountText;
+
+    // Hàm này sẽ được gọi để cập nhật thông tin hiển thị lên ô
+    public void UpdateSlot(InventorySlot slot)
+    {
+        if (slot != null && slot.item != null)
+        {
+            // Bật icon và gán hình ảnh của vật phẩm vào
+            itemIcon.gameObject.SetActive(true);
+            itemIcon.sprite = slot.item.icon;
+
+            // Nếu số lượng > 1 thì mới hiện số, còn 1 cái thì ẩn số đi cho đẹp
+            if (slot.amount > 1)
+            {
+                amountText.gameObject.SetActive(true);
+                amountText.text = slot.amount.ToString();
+            }
+            else
+            {
+                amountText.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            // Nếu ô trống thì tắt icon và chữ đi
+            itemIcon.gameObject.SetActive(false);
+            amountText.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Script/ItemData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public enum ItemCategory { Ammunition, Medical, Consumable }
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class ItemData : ScriptableObject
+{
+    public string itemName;
+    public Sprite icon;
+    public ItemCategory category;
+
+    [Header("Cài đặt Cộng dồn (Stacking)")]
+    public bool isStackable = true;
+    public int maxStack = 30; // Ví dụ: 30 viên đạn 1 ô
+
+    [Header("Chỉ số Tác dụng")]
+    public float healAmount;     // Lượng máu hồi
+    public float staminaRegen;   // Lượng thể lực hồi
+    public float useTime;        // Thời gian chờ khi sử dụng (tạo độ khó hardcore)
+}
